d899a2d7ad390d9ebef4909ee0559351cf70df9f
Do Duc <ducdotrong2000@gmail.com>
2021-08-29 03:07:34 +0700

    Basket + Discount API, Discount Grpc.

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtension.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using Polly;
+using System;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtension
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating postresql database.");
+
+                    var retry = Policy.Handle<NpgsqlException>()
+                            .WaitAndRetry(
+                                retryCount: 5,
+                                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // 2,4,8,16,32 sc
+                                onRetry: (exception, retryCount, context) =>
+                                {
+                                    logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                                });
+
+                    //if the postgresql server container is not created on run docker compose this
+                    //migration can't fail for network related exception. The retry options for database operations
+                    //apply to transient exceptions                    
+                    retry.Execute(() => ExecuteMigrations(configuration));
+
+                    logger.LogInformation("Migrated postresql database.");
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the postresql database");
+                }
+            }
+
+            return host;
+        }
+
+        private static void ExecuteMigrations(IConfiguration configuration)
+        {
+            using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            connection.Open();
+
+            using var command = new NpgsqlCommand
+            {
+                Connection = connection
+            };
+
+            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+            command.ExecuteNonQuery();
+
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('giay1', 'Discount Giày', 150);";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('dep2', 'Discount Dép 2', 100);";
+            command.ExecuteNonQuery();
+        }
+    }
+}
